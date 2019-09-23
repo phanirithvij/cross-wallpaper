7243e2746a4fc5b6fe35d53759b8eec965142ca7
phanirithvij <phanirithvij20003@gmail.com>
2019-09-23 17:28:51 +0530

    mac

--- macos/lib.dart
+++ macos/lib.dart
@@ -6,6 +6,9 @@ main() {
     "-e",
     'tell application "Finder" to get POSIX path of (get desktop picture as alias)'
   ]).then((ProcessResult res) {
-    print(res);
+    print(res.stdout);
   });
+
+//   	return exec.Command("osascript", "-e", `tell application "System Events" to tell every desktop to set picture to `+strconv.Quote(file)).Run()
+
 }
