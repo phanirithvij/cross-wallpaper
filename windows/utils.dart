7fc13c51387b2ea354d970e630559e39217a1ad2
phanirithvij <phanirithvij2000@gmail.com>
2019-11-08 02:33:25 +0530

    warnings

--- windows/utils.dart
+++ windows/utils.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
+/// Downloads a file to a specified path
 Future downloadFile(String url, String filename) async {
   await HttpClient()
       .getUrl(Uri.parse(url))
-      .then((HttpClientRequest request) => request.close())
-      .then((HttpClientResponse response) =>
-          response.pipe(File(filename).openWrite()));
+      .then((request) => request.close())
+      .then((response) => response.pipe(File(filename).openWrite()));
 }
