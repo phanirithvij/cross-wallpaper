fe643818aceaeaf34c0ba15f260785e46ace5c4e
phanirithvij <phanirithvij2000@gmail.com>
2019-11-08 02:29:53 +0530

    remove warnings etc...

--- windows/windows.dart
+++ windows/windows.dart
@@ -2,36 +2,38 @@
 // Uses dart:ffi requires dart>=2.5.0
 
 import 'dart:ffi';
-import 'package:ffi/ffi.dart';
-
-import 'package:path/path.dart' as p;
-import 'utils.dart' show downloadFile;
 import 'dart:io';
 
-const int MAX_PATH = 1000;
+import 'package:ffi/ffi.dart';
+import 'package:path/path.dart' as p;
+import 'utils.dart' show downloadFile;
 
-main(List args) async {
+/// MAX_PATH for a filename
+// ignore: constant_identifier_names
+const MAX_PATH = 1000;
+
+Future<int> main(List args) async {
   if (args.length == 1) {
     final file = p.canonicalize(args[0]);
     final exists =
         FileSystemEntity.typeSync(file) != FileSystemEntityType.notFound;
 
     if (exists && !setWallpaper(file)) {
-      print("Failed to set wallpaper");
+      print('Failed to set wallpaper');
       return -1;
     }
-    print("Not a file");
+    print('Not a file');
     // url
     final directory = await Directory.systemTemp.createTemp('my_temp_dir');
     final path = directory.path;
 
-    downloadFile(args[0], "$path/temp.png").whenComplete(() {
-      setWallpaper(p.canonicalize("$path/temp.png"));
-      exit(0);
+    await downloadFile(args[0], '$path/temp.png').whenComplete(() {
+      setWallpaper(p.canonicalize('$path/temp.png'));
     });
     return 0;
   }
   print(getWallpaper());
+  return 0;
 }
 
 /// [Utf16] Helper class to decode and encode String to Utf16 and back
@@ -41,11 +43,14 @@ extension Utf16String on Pointer<Utf16> {
   /// Get a [String] from a [Pointer<Utf16>] object
   /// [details](https://github.com/dart-lang/ffi/issues/21#issuecomment-550336125)
   String fromUtf16() {
-    final units = List<int>();
+    final units = <int>[];
     var len = 0;
+    // ignore: literal_only_boolean_expressions
     while (true) {
-      final int char = this.cast<Int16>().elementAt(len++).value;
-      if (char == 0 || len > MAX_PATH) break;
+      final char = cast<Int16>().elementAt(len++).value;
+      if (char == 0 || len > MAX_PATH) {
+        break;
+      }
       units.add(char);
     }
     return String.fromCharCodes(units);
@@ -70,6 +75,7 @@ typedef SystemParametersInfoWC = Int8 Function(
 typedef SystemParametersInfoWDart = int Function(
     int uiAction, int uiParam, Pointer pvParam, int fWinIni);
 
+/// Get the wallpaper path
 String getWallpaper() {
   // Load user32.dll.
   final dylib = DynamicLibrary.open('user32.dll');
@@ -80,17 +86,19 @@ String getWallpaper() {
           'SystemParametersInfoW');
 
   // Allocate pointers to Utf16 arrays containing the command arguments.
-  final Pointer<Utf16> filenameP = Utf16.toUtf16("0" * MAX_PATH);
+  final filenameP = Utf16.toUtf16('0' * MAX_PATH);
 
-  const int SPI_GETDESKWALLPAPER = 0x0073;
+  // ignore: constant_identifier_names
+  const SPI_GETDESKWALLPAPER = 0x0073;
   // Invoke the command, and free the pointers.
   systemParWP(SPI_GETDESKWALLPAPER, MAX_PATH, filenameP, 0);
-  String wall = filenameP.fromUtf16();
+  final wall = filenameP.fromUtf16();
   free(filenameP);
 
   return wall;
 }
 
+/// Sets the given filename as the wallpaper
 bool setWallpaper(String filename) {
   // Load user32.dll.
   final dylib = DynamicLibrary.open('user32.dll');
@@ -101,12 +109,16 @@ bool setWallpaper(String filename) {
           'SystemParametersInfoW');
 
   // http://pinvoke.net/default.aspx/Enums/SPIF.html
-  const int SPI_SETDESKWALLPAPER = 0x0014;
-  const int SPIF_UPDATEINIFILE = 0x01;
-  const int SPIF_SENDCHANGE = 0x02;
+
+// ignore: constant_identifier_names
+  const SPI_SETDESKWALLPAPER = 0x0014;
+// ignore: constant_identifier_names
+  const SPIF_UPDATEINIFILE = 0x01;
+// ignore: constant_identifier_names
+  const SPIF_SENDCHANGE = 0x02;
 
   // Allocate pointers to Utf16 arrays containing the command arguments.
-  final Pointer<Utf16> filenameP = Utf16.toUtf16(filename);
+  final filenameP = Utf16.toUtf16(filename);
 
   // Invoke the command, and free the pointers.
   final result = systemParWP(
